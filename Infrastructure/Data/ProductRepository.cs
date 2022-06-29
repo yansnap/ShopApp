f8b75727919bd907925466a5b7c6193d3070377c
JanHappy <jan.kozhyn@gmail.com>
2022-06-29 13:10:31 +0300

    Creating a specification class

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -27,6 +27,13 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
+            // var typeId = 1;
+
+            // var products = _context.Products
+            // .Where(x => x.ProductTypeId == typeId)
+            // .Include(x => x.ProductType)
+            // .ToListAsync();
+
             return await _context.Products
             .Include(p => p.ProductType)
             .Include(p => p.ProductBrand)

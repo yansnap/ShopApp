df425b977b477580328df5ffa7c569ab7d00bca3
JanHappy <jan.kozhyn@gmail.com>
2022-06-30 14:45:50 +0300

    Adding a sorting specification

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -6,7 +6,7 @@ namespace Infrastructure.Data
 {
     public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
     {
-        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec) 
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
         {
             var query = inputQuery;
 
@@ -14,6 +14,17 @@ namespace Infrastructure.Data
             {
                 query = query.Where(spec.Criteria); // id == ProductId
             }
+
+            if (spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy); // id == ProductId
+            }
+
+            if (spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending); // id == ProductId
+            }
+
 
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 

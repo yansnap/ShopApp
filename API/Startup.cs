0d39e320ce6e7af54f6b0eab8a64288bee78884d
JanHappy <jan.kozhyn@gmail.com>
2022-06-30 16:29:53 +0300

    Adding CORS Support to the API

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,7 +27,12 @@ namespace API
             ));
             services.AddApplicationServices();
             services.AddSwaggerDocumentaion();
-
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +43,7 @@ namespace API
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
             app.UserSwaggerDoucmentaion();
             app.UseEndpoints(endpoints =>

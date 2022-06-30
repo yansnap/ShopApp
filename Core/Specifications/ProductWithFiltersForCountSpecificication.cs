bc8d458ded8e83c1b6042ce8d21a6ce6a8bc3fbb
JanHappy <jan.kozhyn@gmail.com>
2022-06-30 16:08:40 +0300

    Adding Pagination pt3

---
+++ Core/Specifications/ProductWithFiltersForCountSpecificication.cs
@@ -0,0 +1,16 @@
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductWithFiltersForCountSpecificication : BaseSpecification<Product>
+    {
+        public ProductWithFiltersForCountSpecificication(ProductSpecParams productParams)
+            :  base(x =>
+                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
+        {
+
+        }
+    }
+}
--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -16,28 +16,27 @@ namespace Infrastructure.Data
         {
             _context = context;
         }
-
         public async Task<T> GetByIdAsync(int id)
         {
             return await _context.Set<T>().FindAsync(id);
         }
-
         public async Task<IReadOnlyList<T>> ListAllAsync()
         {
             return await _context.Set<T>().ToListAsync();
         }
-
         public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
         {
             return await ApplySpecification(spec).FirstOrDefaultAsync();
         }
-
         public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
         {
             return await ApplySpecification(spec).ToListAsync();
         }
-
-        private IQueryable<T> ApplySpecification(ISpecification<T> spec) 
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
             return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }

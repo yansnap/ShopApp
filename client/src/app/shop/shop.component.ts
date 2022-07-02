8a3d61cc510feb37de80cb2054f3d2533085acad
JanHappy <jan.kozhyn@gmail.com>
2022-07-02 19:28:12 +0300

    Adding the sort functionality

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -16,6 +16,13 @@ export class ShopComponent implements OnInit {
   types: IType[];
   brandIdSelected = 0;
   typeIdSelected = 0;
+  sortSelected = 'name';
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to High', value: 'priceAsc'},
+    {name: 'Price: High to Low', value: 'priceDesc'}
+  ];
+
 
   constructor(private shopService: ShopService) { }
 
@@ -26,7 +33,8 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts() {
-    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe(response => {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected, this.sortSelected)
+    .subscribe(response => {
       this.products = response.data;
     }, error => {
       console.log(error)
@@ -56,6 +64,11 @@ export class ShopComponent implements OnInit {
   
   onTypeSelected(typeId: number) {
     this.typeIdSelected = typeId;
+    this.getProducts();
+  }
+
+  onSortSelected(sort: string) {
+    this.sortSelected = sort;
     this.getProducts();
   }
 

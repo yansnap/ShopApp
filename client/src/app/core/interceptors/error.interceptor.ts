9e649398ad0acbac4741de3d8d1457581a625a93
JanHappy <jan.kozhyn@gmail.com>
2022-07-04 16:07:28 +0300

    Adding toast notifications

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -8,23 +8,30 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastr: ToastrService) {}
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
-      catchError(error => {
+      catchError((error) => {
         if (error) {
-          if(error.status === 404) {
-            this.router.navigateByUrl('/not-found')
+          if (error.status === 400) {
+            this.toastr.error(error.error.message, error.error.statusCode);
           }
-          if(error.status === 500) {
-            this.router.navigateByUrl('/server-error')
+          if (error.status === 400) {
+            this.toastr.error(error.error.message, error.error.statusCode);
+          }
+          if (error.status === 404) {
+            this.router.navigateByUrl('/not-found');
+          }
+          if (error.status === 500) {
+            this.router.navigateByUrl('/server-error');
           }
         }
         return throwError(error);

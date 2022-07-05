8c8d41bf01de8b60ba8959c4ea0267859ed36b84
JanHappy <jan.kozhyn@gmail.com>
2022-07-05 13:21:52 +0300

    Cleaning up loading issues

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -12,7 +12,7 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
-  @ViewChild('search', {static: true}) searchItem: ElementRef;
+  @ViewChild('search', {static: false}) searchItem: ElementRef;
   products: IProduct[];
   brands: IBrand[];
   types: IType[];

1ed785ab7279a451046a6590f9f875e6c537562d
JanHappy <jan.kozhyn@gmail.com>
2022-07-06 20:52:21 +0300

    Troubleshooting auth issues

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -27,6 +27,7 @@ namespace API.Extensions
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"])),
                         ValidIssuer = config["Token:Issuer"],
                         ValidateIssuer = true,
+                        ValidateAudience = false
                     };
                 });
 

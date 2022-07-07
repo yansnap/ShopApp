732902f2d12ef9fa4fb3f115e29efa3990ee0333
JanHappy <jan.kozhyn@gmail.com>
2022-07-07 12:18:24 +0300

    Creating the account module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,26 +7,51 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
-  { path: 'test-error', component: TestErrorComponent, data: { breadcrumb: 'Test Errors' } },
-  { path: 'server-error', component: ServerErrorComponent, data: { breadcrumb: 'Server Error' } },
-  { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'Not found' } },
   {
-    path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
-    data: { breadcrumb: 'Shop' }
+    path: 'test-error',
+    component: TestErrorComponent,
+    data: { breadcrumb: 'Test Errors' },
   },
   {
-    path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule),
-    data: { breadcrumb: 'Basket' }
+    path: 'server-error',
+    component: ServerErrorComponent,
+    data: { breadcrumb: 'Server Error' },
   },
   {
-    path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule),
-    data: { breadcrumb: 'Checkout' }
+    path: 'not-found',
+    component: NotFoundComponent,
+    data: { breadcrumb: 'Not found' },
   },
-  { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
+  {
+    path: 'shop',
+    loadChildren: () =>
+      import('./shop/shop.module').then((mod) => mod.ShopModule),
+    data: { breadcrumb: 'Shop' },
+  },
+  {
+    path: 'basket',
+    loadChildren: () =>
+      import('./basket/basket.module').then((mod) => mod.BasketModule),
+    data: { breadcrumb: 'Basket' },
+  },
+  {
+    path: 'checkout',
+    loadChildren: () =>
+      import('./checkout/checkout.module').then((mod) => mod.CheckoutModule),
+    data: { breadcrumb: 'Checkout' },
+  },
+  {
+    path: 'account',
+    loadChildren: () =>
+      import('./account/account.module').then((mod) => mod.AccountModule),
+    data: { breadcrumb: {skip:true} },
+  },
+  
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

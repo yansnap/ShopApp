9df7a151aa0c1b3fbe5fc253ec1e69f10340d60e
JanHappy <jan.kozhyn@gmail.com>
2022-07-07 16:07:06 +0300

    Creating a register form

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -7,13 +7,12 @@ import { AccountService } from '../account.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-
-  constructor(private accountService: AccountService, private router: Router) { }
+  constructor(private accountService: AccountService, private router: Router) {}
 
   ngOnInit(): void {
     this.createLoginForm();
@@ -21,17 +20,22 @@ export class LoginComponent implements OnInit {
 
   createLoginForm() {
     this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]),
-      password: new FormControl('', Validators.required)
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
+      ]),
+      password: new FormControl('', Validators.required),
     });
   }
 
   onSubmit() {
-    this.accountService.login(this.loginForm.value).subscribe(() => {
-      this.router.navigateByUrl('/shop');
-    }, error => {
-      console.log(error);
-    })
+    this.accountService.login(this.loginForm.value).subscribe(
+      () => {
+        this.router.navigateByUrl('/shop');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }

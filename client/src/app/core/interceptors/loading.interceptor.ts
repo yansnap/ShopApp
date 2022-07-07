43a67b05866b6c54920ff5cd72dccfe9441e1397
JanHappy <jan.kozhyn@gmail.com>
2022-07-07 16:48:45 +0300

    Impoving the async validator

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -14,9 +14,13 @@ export class LoadingInterceptor implements HttpInterceptor {
 
   constructor(private busyService: BusyService) { }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!req.url.includes('emailexists')) {
+      this.busyService.busy();
+    }
+
     this.busyService.busy();
-    return next.handle(request).pipe(
+    return next.handle(req).pipe(
       delay(1000),
       finalize(() => {
         this.busyService.idle();

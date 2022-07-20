d36e79bba12312e4f749e75908abfd3e7b3aad5a
JanHappy <jan.kozhyn@gmail.com>
2022-07-20 20:39:19 +0300

    Creating the order migration

--- Core/Entities/OrderAggregate/Order.cs
+++ Core/Entities/OrderAggregate/Order.cs
@@ -5,6 +5,10 @@ namespace Core.Entities.OrderAggregate
 {
     public class Order : BaseEntity
     {
+        public Order()
+        {
+        }
+
         public Order(string buyerEmail, Address shipToAddress, DeliveryMethod deliveryMethod, IReadOnlyList<OrderItem> orderItems, decimal subtotal)
         {
             BuyerEmail = buyerEmail;

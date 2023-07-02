4940371b30b4bd43e10ca6537bbf09aa16ac8223
Yan <jan.kozhyn@gmail.com>
2023-07-02 11:50:04 +0300

    Setting up Entity Framework

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
